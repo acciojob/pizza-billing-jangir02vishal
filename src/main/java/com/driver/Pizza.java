8ffbb5205cbe3098122995d1c548c5c6542b0b0e
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-01-26 21:49:32 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,50 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+    protected int price;
+    protected Boolean isVeg;
+    protected boolean extraCheeseAdded = false;
+    protected boolean extraToppingsAdded = false;
+    protected boolean takeawayAdded = false;
+    protected StringBuilder bill = new StringBuilder();
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-        // your code goes here
+        this.price = isVeg ? 300 : 400;
+        bill.append("Base Price Of The Pizza: ").append(this.price).append("\n");
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese() {
+        if (!extraCheeseAdded) {
+            this.price += 80;
+            extraCheeseAdded = true;
+            bill.append("Extra Cheese Added: 80\n");
+        }
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings() {
+        if (!extraToppingsAdded) {
+            int toppingsPrice = isVeg ? 70 : 120;
+            this.price += toppingsPrice;
+            extraToppingsAdded = true;
+            bill.append("Extra Toppings Added: ").append(toppingsPrice).append("\n");
+        }
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway() {
+        if (!takeawayAdded) {
+            this.price += 20;
+            takeawayAdded = true;
+            bill.append("Paperbag Added: 20\n");
+        }
     }
 
-    public String getBill(){
-        // your code goes here
-        return this.bill;
+    public String getBill() {
+        bill.append("Total Price: ").append(this.price);
+        return bill.toString();
     }
 }
