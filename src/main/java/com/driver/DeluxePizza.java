ff10e28c899cd616bdf84f6a60f40679d497ad3e
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-01-26 21:50:47 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,9 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        addExtraCheese();
+        addExtraToppings();
+        // Since DeluxePizza includes extra cheese and toppings by default,
+        // we call these methods in the constructor to automatically add them to the pizza.
     }
 }
