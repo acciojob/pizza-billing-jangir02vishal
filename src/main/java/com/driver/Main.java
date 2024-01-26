b4291914f932fe4bfbc685f0c2c0e2f4b6db4f45
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-01-26 21:50:25 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,18 @@
 package com.driver;
 
 public class Main {
-  public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
-    System.out.println(dp.getBill());
+    public static void main(String[] args) {
+        DeluxePizza dp = new DeluxePizza(true);
+        System.out.println(dp.getPrice());
+        dp.addTakeaway();
+        dp.addExtraCheese(); // This should not add extra cheese again as it's already included
+        dp.addTakeaway(); // This should not add another takeaway bag as it's already included
+        System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
-  }
+        Pizza p = new Pizza(true);
+        p.addExtraCheese();
+        p.addExtraToppings();
+        p.addTakeaway();
+        System.out.println(p.getBill());
+    }
 }
